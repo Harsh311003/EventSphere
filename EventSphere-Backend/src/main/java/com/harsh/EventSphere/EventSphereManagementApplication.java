24e7adee6f4c94eb9fb4a38b24f28c78b97eb152
Harsh311003 <harshsharma311003@gmail.com>
2025-05-28 21:37:11 +0530

    Initial setup done

---
+++ EventSphere-Backend/src/main/java/com/harsh/EventSphere/EventSphereManagementApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.EventSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class EventSphereManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println("====================== hello ======================");
+		SpringApplication.run(EventSphereManagementApplication.class, args);
+	}
+
+}
