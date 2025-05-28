bbde45c0a9ef2d20070efb616a3e27478ecfdde6
Harsh311003 <harshsharma311003@gmail.com>
2025-05-28 22:26:17 +0530

    User entity, Role enum, and repository interface created

--- EventSphere-Backend/src/main/java/com/harsh/EventSphere/EventSphereManagementApplication.java
+++ EventSphere-Backend/src/main/java/com/harsh/EventSphere/EventSphereManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EventSphereManagementApplication {
 
 	public static void main(String[] args) {
-		System.out.println("====================== hello ======================");
+//		System.out.println("====================== Event Sphere ======================");
 		SpringApplication.run(EventSphereManagementApplication.class, args);
 	}
 
